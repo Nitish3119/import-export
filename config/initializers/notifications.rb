f1754a35e75bf99f3254565637ddca0f1a601b58
Ongraph <nitish.agarwal@ongraph.com>
2023-05-03 16:43:48 +0530

    minor change

--- config/initializers/notifications.rb
+++
@@ -1,4 +0,0 @@
-Notifications.define :success, class: 'success-notification', duration: 3000
-Notifications.define :error, class: 'error-notification', duration: 5000
-Notifications.define :warning, class: 'warning-notification', duration: 4000
-Notifications.define :info, class: 'info-notification', duration: 2000
